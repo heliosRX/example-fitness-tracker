3bbc418551342c4b7ca97acb1a20ef6ea9ccc4b0
Thomas <tw@goalpilot.com>
2020-01-09 22:06:24 -0500

    :art: simplified mainjs

--- src/main.js
+++ src/main.js
@@ -1,24 +1,19 @@
 import Vue from 'vue'
-import heliosRX from 'heliosrx'
 import App from './App.vue'
 
 import router from './router'
 
+import heliosRX from 'heliosrx'
 import { rtdb } from './firebase' // Import realtime database
 import models from "@/models"
-import api from 'heliosrx/src/api'
-import GenericStore from 'heliosrx/src/store'
 
 Vue.config.productionTip = false
 
 Vue.use(heliosRX, {
-  models: models,
+  models:  models,
+  db:      rtdb,
+  devMode: true,
 })
-
-GenericStore.setDefaultDB( rtdb );
-api.get_registry().commit('INIT_REGISTRY');
-
-window.$models = models;
 
 new Vue({
   router,
