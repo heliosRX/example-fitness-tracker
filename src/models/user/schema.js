333db31df08f4a23c042bb13b750643f21ead35b
Thomas <tw@goalpilot.com>
2020-01-30 12:00:05 -0500

    :card_index: minor update schema

--- src/models/user/schema.js
+++ src/models/user/schema.js
@@ -8,12 +8,13 @@ export default {
       phonenumber: '',
     };
   },
-  unsafe_disable_validation: true,
+  // unsafe_disable_validation: true,
   fields: {
     username:    { validate_bolt_type: 'String', required: true },
     createdAt:   { validate_bolt_type: 'ServerTimestamp' },
     phonenumber: { validate_bolt_type: 'String' },
     groups:      { validate_bolt_type: 'Map<AnyID, String>' },
+    mytype:      { validate_bolt_type: 'Type []' },
     // groups:      { validate_bolt_type: 'Any' },
   },
 };
